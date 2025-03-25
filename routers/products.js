dd811d8c3b0e12c8b4079c5dafc417e5ba5d13ec
Alex Mihov <alexmihov.as@gmail.com>
2025-03-25 17:00:30 +0100

    feat: add showCategory and showSearchBar functions to productsController and update routes

--- routers/products.js
+++ routers/products.js
@@ -1,11 +1,14 @@
 const express = require('express');
-const { index, show, storeReview } = require('../controllers/productsController');
+const { index, show, storeReview, showCategory, showSearchBar } = require('../controllers/productsController');
 const router = express.Router();
 
 
 router.get('/', index);
 router.get('/:id', show);
 
+router.get('/search', showSearchBar);
+
+router.get('/category/:id', showCategory);
 
 router.post('/:id/reviews/create', storeReview)
 
